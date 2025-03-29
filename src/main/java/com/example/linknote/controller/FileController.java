5c5d80dceb78c7fc47c4af9b5b1a0801e6adae37
tomacx <cxh@cxhdeMacBook-Pro.local>
2025-03-29 11:03:36 +0800

    修改文件路径python调用

--- src/main/java/com/example/linknote/controller/FileController.java
+++ src/main/java/com/example/linknote/controller/FileController.java
@@ -69,7 +69,7 @@ public class FileController {
             // 调用Python脚本插入PDF到数据库
             ProcessBuilder pb = new ProcessBuilder("/home/ubuntu/app/python_back/.venv/bin/python",
                     "/home/ubuntu/app/python_back/insert.py",  // 使用绝对路径
-                    "http://82.157.18.189/uploads/"+filePath,
+                    "/home/ubuntu/app/uploads/"+filePath,
                     userId.toString());
 
             Process process = pb.start();
